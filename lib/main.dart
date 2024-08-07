444e57104d932ee050e3abe513c5ae08cb61a8db
eskarasu <e.s.krs000@gmail.com>
2024-08-07 12:00:22 +0300

    "Refactor main.dart to use a StatefulWidget for form handling, add TurquoisePage, and update pubspec.yaml to include image asset; update widget_test.dart to use non-const MyApp instance"

--- lib/main.dart
+++ lib/main.dart
@@ -4,23 +4,69 @@ void main() {
   runApp(MyApp());
 }
 
-// MyApp is a StatelessWidget which renders the home screen of the application.
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // MaterialApp is the root widget of the application.
     return MaterialApp(
-      // Scaffold provides the structure of the home screen.
       home: Scaffold(
-        // AppBar is the header bar of the app, containing the title.
         appBar: AppBar(
           title: Text('Hello World Application'),
         ),
-        // The body of the Scaffold is a Center widget containing a Text widget.
-        body: Center(
-          // The Text widget displays 'Hello World!' on the screen.
-          child: Text('Hello World!'),
-        ),
+        body: MyCustomForm(),
+      ),
+    );
+  }
+}
+
+// Creating a new StatefulWidget to track the state of form fields.
+class MyCustomForm extends StatefulWidget {
+  @override
+  _MyCustomFormState createState() => _MyCustomFormState();
+}
+
+class _MyCustomFormState extends State<MyCustomForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        // ...
+        children: <Widget>[
+          // ...
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                // Safely calling the 'validate' method by adding null check.
+                if (_formKey.currentState?.validate() ?? false) {
+                  // If the form is valid, navigate to a new page with turquoise background.
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => TurquoisePage()),
+                  );
+                }
+              },
+              child: Text('Login'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TurquoisePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.cyan, // Turquoise color
+      appBar: AppBar(
+        title: Text('Turquoise Page'),
+      ),
+      body: Center(
+        child: Image.asset('assets/images/image.png'), // Centering the image
       ),
     );
   }
